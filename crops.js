6b97e91c941c5b7d6abcf7070674c437743303d2
mswhts <mohamedelm774@gmail.com>
2026-09-19 16:04:02 +0930

    Update crops.js

---
+++ crops.js
@@ -0,0 +1,28 @@
+let crops = {
+    wheat: {
+        name: "wheat",
+        stages: ["🌱", "🌿", "🌾"],
+        emoji: "🌾",
+        growthTime: 48,
+        idealMoisture: 60,
+        health: 100
+    },
+
+    corn: {
+        name: "corn",
+        stages: ["🌱", "🌿", "🌽"],
+        emoji: "🌽",
+        growthTime: 60,
+        idealMoisture: 70,
+        health: 100
+    },
+
+    carrot: {
+        name: "carrot",
+        stages: ["🌱", "🌿", "🥕"],
+        emoji: "🥕",
+        growthTime: 36,
+        idealMoisture: 65,
+        health: 100
+    }
+};
